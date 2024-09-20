f12cbf7c286e33df84b0df9b435930c26a439dbf
Vlad Semenenko <vlad_semenenko_2016@mail.ru>
2024-09-20 21:46:25 +0300

    feat: basic functionality comleted

--- index.js
+++ index.js
@@ -2,23 +2,44 @@ console.log(123);
 
 const imgs = document.querySelectorAll('.img-container img');
 const searchBar = document.querySelector('.search-bar');
+const searchBarClearButton = document.querySelector('.search-bar-clear');
+const searchBarSearchButton = document.querySelector('.search-bar-search');
 
-async function getPicts(url) {
+searchBarClearButton.addEventListener('click', HandleClearSearch);
+searchBarSearchButton.addEventListener('click', HandleSearch);
+
+searchBar.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        HandleSearch();
+    }
+});
+
+window.onload = searchBar.focus();
+
+let picts = displayPicts(`https://api.unsplash.com/photos/random?count=${imgs.length}&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`);
+
+for(let i = 0; i < imgs.length; i++){
+    imgs[i].addEventListener('click', function(){
+        console.log(i);
+    });
+}
+
+async function displayPicts(url) {
     const response = await fetch(url);
     const picts = await response.json();
     console.log(response);
     console.log(picts);
-    for(let i = 0; i < 12; i++){
+    for(let i = 0; i < imgs.length; i++){
         imgs[i].src = picts[i].urls.regular;
     }
-    //return picts;
 }
-let picts = getPicts("https://api.unsplash.com/photos/random?count=12&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo");
-//console.log(picts);
 
 function HandleSearch(){
-
+    for(let i = 0; i < imgs.length; i++){
+        imgs[i].src = "assets/imgs/empty-card.png";
+    }
+    displayPicts(`https://api.unsplash.com/photos/random?count=${imgs.length}&query=${searchBar.value}&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`);
 }
 function HandleClearSearch(){
-
+    searchBar.value = '';
 }
